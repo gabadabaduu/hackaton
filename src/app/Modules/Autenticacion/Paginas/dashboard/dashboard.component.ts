8746331620bea48f47bd87fe31b9184b93155f71
juafra4 <juafra454@outlook.com>
2024-04-27 21:17:49 -0500

    Navbar en pantallas de Autenticación y Usuario

--- src/app/Modules/Autenticacion/Paginas/dashboard/dashboard.component.ts
+++ src/app/Modules/Autenticacion/Paginas/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PasosComponent } from '../../Componentes/pasos/pasos';
+import {BarraNavegacionComponent} from '../../Componentes/barra-navegacion/barra-navegacion.component';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [PasosComponent],
+  imports: [PasosComponent, BarraNavegacionComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/Modules/Usuario/Paginas/mis-reservas/mis-reservas.component.ts
+++ src/app/Modules/Usuario/Paginas/mis-reservas/mis-reservas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {BarraNavegacionComponent} from '../../Componentes/barra-navegacion/barra-navegacion.component';
 
 @Component({
   selector: 'app-mis-reservas',
   standalone: true,
-  imports: [],
+  imports: [BarraNavegacionComponent],
   templateUrl: './mis-reservas.component.html',
   styleUrl: './mis-reservas.component.css'
 })
